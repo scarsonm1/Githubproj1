9d827cb6a919f0113a1891a1e9ac67e854da63ca
Shir <healthylegacy01@gmail.com>
2018-08-09 13:14:10 -0500

    Refactored Suctract LOC

--- ConsoleApp4/Calaculator.cs
+++ ConsoleApp4/Calaculator.cs
@@ -6,5 +6,21 @@ namespace ConsoleApp4
 {
     class Calaculator
     {
+        public int Add (int a, int b)
+        {
+             return a+b
+        }
+        public int Subtract (int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+        public int Multiply (int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide (int a, int b)
+        {
+            return 0;
+        }
     }
 }
